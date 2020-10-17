b584346bc9bbf82ac3628a5def18a8247963360a
Gazorby <gazorby@pm.me>
2020-10-18 01:15:39 +0200

    ✨ feat: add chezmoi diff abbr

--- conf.d/fish_chezmoi.fish
+++ conf.d/fish_chezmoi.fish
@@ -2,6 +2,7 @@ abbr -a cm 'chezmoi'
 abbr -a cma 'chezmoi add'
 abbr -a cmap 'chezmoi apply'
 abbr -a cmu 'chezmoi update'
+abbr -a cmd 'chezmoi diff'
 
 
 function __fish_chezmoi_uninstall --on-event fish_chezmoi_uninstall
@@ -9,4 +10,5 @@ function __fish_chezmoi_uninstall --on-event fish_chezmoi_uninstall
     abbr --erase cma
     abbr --erase cmap
     abbr --erase cmu
+    abbr --erase cmd
 end
