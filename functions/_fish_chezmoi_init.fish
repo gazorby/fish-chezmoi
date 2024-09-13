ad74ec686826b1edc922a4b3cb2af673a3a3a73c
Alper Cugun <github@alper.nl>
2024-09-13 20:27:05 +0200

    Add abbr for edit apply

--- functions/_fish_chezmoi_init.fish
+++ functions/_fish_chezmoi_init.fish
@@ -2,6 +2,7 @@ function _fish_chezmoi_init -d "Create chezmoi abbreviation"
     _fish_chezmoi_abbr cm chezmoi
     _fish_chezmoi_abbr cma 'chezmoi add'
     _fish_chezmoi_abbr cmap 'chezmoi apply'
+    _fish_chezmoi_abbr cmea 'chezmoi edit --apply'
     _fish_chezmoi_abbr cmu 'chezmoi update'
     _fish_chezmoi_abbr cmd 'chezmoi diff'
 
